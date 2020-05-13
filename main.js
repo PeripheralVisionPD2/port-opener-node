e7506309b21cc421648f3bde26abdce3db3ab2ba
PeripheralVisionPD2 <jaypoopys@gmail.com>
2020-05-12 20:26:46 -0400

    prettified

--- main.js
+++ main.js
@@ -5,24 +5,22 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-rl.question("[PORT OPENNER] What port would you like to open?: ",(res)=>{
+rl.question("[PORT OPENNER] What port would you like to open?: ", (res) => {
     res = parseInt(res)
     console.log("[PORT OPENNER] Attempting to open port " + res.toString())
-    client.map(res,(err)=>{
-        if(err)
-        {
+    client.map(res, (err) => {
+        if (err) {
             console.log("[PORT OPENNER] An error occured while mapping the port!")
             return
         }
         console.log("[PORT OPENNER] Port successfully mapped!")
-        rl.question("[PORT OPENNER] Press ENTER to unmap and exit.", ()=>{
-            client.unmap(res, (err)=>{
-                if(err)
-                {
+        rl.question("[PORT OPENNER] Press ENTER to unmap and exit.", () => {
+            client.unmap(res, (err) => {
+                if (err) {
                     console.log("[PORT OPENNER] An error occured while unmapping the port!")
                 }
                 console.log("[PORT OPENNER] Port successfully unmapped!")
-                client.destroy(()=>{
+                client.destroy(() => {
                     console.log("[PORT OPENNER] Successfully destroyed client, bye!")
                     process.exit()
                 })
